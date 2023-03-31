7f2a77c55803a16ecd2c18bb9e9472cf8bdcc054
ihengc <ihengc@163.com>
2023-03-31 17:59:47 +0800

    合并排序算法描述分析与实现。

---
+++ experimental/sort/merge.go
@@ -0,0 +1,45 @@
+package sort
+
+/*
+* @author: Chen Chiheng
+* @date: 2023/3/31 0031 17:19
+* @description:
+**/
+
+// 本文件对归并排序算法进行描述分析和实现。
+
+// MergeSort 归并排序
+// low 为子集合的下界，high为子集合的上界。
+func MergeSort(x []int, low, high int) {
+	if low >= high {
+		return
+	}
+	middle := low + (high-low)/2 // middle 为进一步分隔子集合（分解为子问题）。
+	MergeSort(x, low, middle)    // 对子问题进行求解。
+	MergeSort(x, middle+1, high)
+	merge(x, low, middle, high) // 合并子问题求得的解。
+}
+
+func merge(x []int, low int, middle int, high int) {
+	tLow := low
+	tMiddle := middle + 1
+	tX := make([]int, 0)
+	for tLow <= middle && tMiddle <= high {
+		if x[tLow] > x[tMiddle] {
+			tX = append(tX, x[tMiddle])
+			tMiddle++
+		} else {
+			tX = append(tX, x[tLow])
+			tLow++
+		}
+	}
+	if tLow > middle && tMiddle <= high {
+		tX = append(tX, x[tMiddle:high+1]...)
+	}
+	if tMiddle > high && tLow <= middle {
+		tX = append(tX, x[tLow:middle+1]...)
+	}
+	for i := 0; i < len(tX); i++ {
+		x[low+i] = tX[i]
+	}
+}
---
+++ experimental/sort/merge_test.go
@@ -0,0 +1,36 @@
+package sort
+
+import (
+	"testing"
+)
+
+/*
+* @author: Chen Chiheng
+* @date: 2023/3/31 0031 17:25
+* @description:
+**/
+
+type testMergeList []int
+
+func (t testMergeList) Len() int {
+	return len(t)
+}
+
+func (t testMergeList) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t testMergeList) Less(i, j int) bool {
+	return t[i] < t[j]
+}
+
+func TestMerge(t *testing.T) {
+	il := []int{9, 0, 1, 8, 4, 7, 2, 6, 3}
+	tl := testMergeList(il)
+	MergeSort(tl, 0, len(tl)-1)
+	for i := 1; i < tl.Len(); i++ {
+		if tl.Less(i, i-1) {
+			t.Fatal("Merge error.")
+		}
+	}
+}
